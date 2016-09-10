2cfaa60aa11574ea02e63aa096be8b7c8b8a4ae2
Alvaro Miranda Aguilera <kikitux@gmail.com>
2016-09-10 11:03:16 -0400

    added git user name and email to init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,8 @@
 # curl -sSL https://raw.githubusercontent.com/kikitux/home-kikitux/master/init.sh | bash
 
 git init
+git config --global user.email "kikitux@gmail.com"
+git config --global user.name "Alvaro Miranda Aguilera"
 git remote add origin git@github.com:kikitux/home-kikitux
 git fetch origin
 rm .bashrc
