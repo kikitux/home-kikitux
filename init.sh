f63366dc06e3a2b84f50417aa035de33c094185d
Alvaro Miranda Aguilera <kikitux@users.noreply.github.com>
2016-09-10 14:45:00 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,3 +5,5 @@ git remote add origin https://github.com/kikitux/home-kikitux
 git fetch origin
 rm .bashrc
 git checkout -b master --track origin/master
+git submodule update --init --recursive
+source .bash_profile
