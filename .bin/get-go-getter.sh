85949c74b2376908bac7b0d14aa220eb7bde38a0
Alvaro Miranda Aguilera <kikitux@gmail.com>
2016-09-24 14:31:57 +0200

    update

--- .bin/get-go-getter.sh
+++
@@ -1,9 +0,0 @@
-
-UNAMES="`uname -s`"
-
-if [ -f ~/.bin/${UNAMES}/go-getter ];then
-  echo "we good"
-else
-  echo "need to do something about it"
-  #cp Downloads/gopath/bin/go-getter ~/.bin/Darwin/
-fi
