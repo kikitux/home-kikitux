ddd8210dfc0b5a2ea1306f99d07a2da0375e5507
Alvaro Miranda Aguilera <kikitux@gmail.com>
2020-04-18 12:40:19 +0200

    update clean

--- clean.sh
+++ clean.sh
@@ -1,1 +1,11 @@
-rm -fr .git* .vim .vimrc .code LICENSE init.sh clean.sh
+#!/usr/bin/env bash
+
+# curl -sSL https://raw.githubusercontent.com/kikitux/home-kikitux/master/clean.sh | bash
+
+if [ `pwd` == ~ ] ; then
+  rm -fr .git .vim .vimrc .code LICENSE init.sh clean.sh env.sh
+else
+  echo warn: run this script from your ~
+  echo warn: current directory `pwd`
+fi
+
